dca54e9d7b7bbdc68fba588762a3b31f09a802e7
haroldsrivera <snader2003r.c@gmail.com>
2023-09-18 19:44:11 -0500

    interfaces commit

---
+++ app/src/main/java/ue/edu/co/gestorenvios/Solicitar_mercancia.java
@@ -0,0 +1,14 @@
+package ue.edu.co.gestorenvios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Solicitar_mercancia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitar_mercancia);
+    }
+}
