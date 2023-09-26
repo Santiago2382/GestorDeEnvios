41e815006d9eded1fe1607fafbc15fffe995f534
haroldsrivera <snader2003r.c@gmail.com>
2023-09-26 16:50:47 -0500

    registro

--- app/src/main/java/ue/edu/co/gestorenvios/Login.java
+++ app/src/main/java/ue/edu/co/gestorenvios/Login.java
@@ -6,6 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
@@ -19,8 +32,16 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Iniciar la segunda actividad aquí
+
+                EditText etCorreo = findViewById(R.id.etCorreoElectronico);
+                EditText etPass = findViewById(R.id.etLoginPass);
+                String usuario = etCorreo.getText().toString();
+                String contrasena = etPass.getText().toString();
+
+
                 Intent intent = new Intent(Login.this, OlvidoPass.class);
                 startActivity(intent);
+                loginUser(usuario, contrasena);
             }
         });
 
@@ -44,6 +65,40 @@ public class Login extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void loginUser(String usuario, String contrasena) {
+        String url = "http://192.168.1.10/api_gestor/login.php";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url, response -> {
+            // Respuesta del servidor
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                String message = jsonResponse.getString("message");
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+                if (jsonResponse.has("user")) {
+                    // Aquí puedes manejar la información del usuario si es necesario
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            // Error en la solicitud
+            Toast.makeText(getApplicationContext(), "Error al conectar", Toast.LENGTH_SHORT).show();
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("usuario", usuario);
+                params.put("contrasena", contrasena);
+                return params;
+            }
+        };
+
+        queue.add(postRequest);
     }
 
 }
--- app/src/main/java/ue/edu/co/gestorenvios/Registro.java
+++ app/src/main/java/ue/edu/co/gestorenvios/Registro.java
@@ -1,30 +1,97 @@
-
 package ue.edu.co.gestorenvios;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Registro extends AppCompatActivity {
+
+    // Declaraciones de los campos de entrada
+    EditText etUsuario, etPassword, etNumeroCelular, etNit, etCorreo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        // Asociación de campos de entrada
+        etUsuario = findViewById(R.id.etUsuarioRegistro);
+        etPassword = findViewById(R.id.etPassResgistro);
+        etNumeroCelular = findViewById(R.id.etNumeroCelular);
+        etNit = findViewById(R.id.etNit);
+        etCorreo = findViewById(R.id.etCorreo);
+
+        Button buttonRegistro = findViewById(R.id.btnSiguienteRegis);
+        buttonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
         Button buttonDevolverce = findViewById(R.id.btnDevolverce);
         buttonDevolverce.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Iniciar la segunda actividad aquí
                 Intent intent = new Intent(Registro.this, Login.class);
                 startActivity(intent);
             }
         });
     }
 
+    public void registerUser() {
+
+        String url = "http://192.168.1.10/api_gestor/register.php";
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url, response -> {
+
+
+            // Respuesta del servidor
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                String message = jsonResponse.getString("message");
+                Toast.makeText(Registro.this, message, Toast.LENGTH_SHORT).show();
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(Registro.this, "JSONException: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }, error -> {
+            // Error en la solicitud
+            String errorMessage = error != null ? error.toString() : "Unknown error";
+            Toast.makeText(Registro.this, "Error al conectar: " + errorMessage, Toast.LENGTH_SHORT).show();
+        }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("usuario", etUsuario.getText().toString());
+                params.put("password", etPassword.getText().toString());
+                params.put("numero_celular", etNumeroCelular.getText().toString());
+                params.put("nit_o_cc", etNit.getText().toString());
+                params.put("correo", etCorreo.getText().toString());
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(postRequest);
+    }
 }
+
