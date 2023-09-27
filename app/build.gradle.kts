96b34927c45aff3b75eaa8c499cfccad51c4d4ee
Santiago Andrade <santiagoandrade864@gmail.com>
2023-09-26 22:24:33 -0500

    Recuperar contraseña

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("androidx.core:core:1.5.0")
-
+    implementation ("com.android.volley:volley:1.2.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:3.5.0")
--- app/src/main/java/ue/edu/co/gestorenvios/OlvidoPass.java
+++ app/src/main/java/ue/edu/co/gestorenvios/OlvidoPass.java
@@ -1,19 +1,33 @@
 package ue.edu.co.gestorenvios;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OlvidoPass extends AppCompatActivity {
+
+    private EditText etCorreo, etNuevaPass, etConfirmarPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_olvido_pass);
+
+        etCorreo = findViewById(R.id.etCorreo);
+        etNuevaPass = findViewById(R.id.etNuevaPass);
+        etConfirmarPass = findViewById(R.id.etConfirmarPass);
 
         ImageButton buttonDevolverse = findViewById(R.id.btnOlvidoPassDevolverse);
         buttonDevolverse.setOnClickListener(view -> {
@@ -22,13 +36,45 @@ public class OlvidoPass extends AppCompatActivity {
         });
 
         Button buttonConfirmar = findViewById(R.id.btnConfirmarPass);
-        buttonConfirmar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(OlvidoPass.this, Login.class);
-                startActivity(intent);
-            }
-        });
+        buttonConfirmar.setOnClickListener(view -> {
+            String correo = etCorreo.getText().toString().trim();
+            String nuevaPass = etNuevaPass.getText().toString().trim();
+            String confirmarPass = etConfirmarPass.getText().toString().trim();
 
+            cambiarContrasena(correo, nuevaPass, confirmarPass);
+        });
+    }
+
+    private void cambiarContrasena(String correo, String nuevaPass, String confirmarPass) {
+        String URL = "http://192.168.1.8/api_gestor/nueva_contrasena.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        int success = jsonObject.getInt("success");
+                        String message = jsonObject.getString("message");
+
+                        if(success == 1) {
+                            Toast.makeText(OlvidoPass.this, message, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(OlvidoPass.this, message, Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(OlvidoPass.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                },
+                error -> Toast.makeText(OlvidoPass.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show()) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("correo", correo);
+                params.put("nuevaPass", nuevaPass);
+                params.put("confirmarPass", confirmarPass);
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(stringRequest);
     }
 }
